e0499707237570701e479d160d7a409629d63a8e
Corey McKinnon <cmckinnon@stampinup.com>
2022-12-05 15:19:22 -0700

    initial commit

---
+++ 2022/01/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Advent01
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            IOrderedEnumerable<int> sorted = File.ReadAllText("data.txt")
+                .Split(new[] { "\n\n" }, StringSplitOptions.None)
+                .Select(g => g.Split('\n', StringSplitOptions.RemoveEmptyEntries)
+                .Select(d => int.Parse(d)))
+                .Select(d => d.Sum(n => n))
+                .OrderByDescending(p => p);
+
+            // 68802
+            Console.WriteLine("part 1:" + sorted
+                .First());
+
+            // 205370
+            Console.WriteLine("part 2: " + sorted
+                .Take(3)
+                .Sum());
+        }
+    }
+}
