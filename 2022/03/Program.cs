5a50b083c91227d790ef5475ae8db0463d72f8a7
Corey McKinnon <cmckinnon@stampinup.com>
2022-12-06 08:20:05 -0700

    day 3 part 1

---
+++ 2022/03/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Advent03
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            string[] sacks = File.ReadAllLines("data.txt");
+
+            int sum = 0;
+
+            foreach (string sack in sacks)
+            {
+                string comp1 = sack[..(sack.Length / 2)];
+                string comp2 = sack[(sack.Length / 2)..];
+
+                char shared = comp1.Intersect(comp2).First();
+
+                sum += shared - (char.IsLower(shared) ? 'a' - 1 : 'A' - 27);
+            }
+
+            Console.WriteLine($"Part 1: {sum}");
+        }
+    }
+}
