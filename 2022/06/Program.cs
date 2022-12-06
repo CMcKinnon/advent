b6fa6f69430f793b2eaf6188f76e16f58ca49bcd
Corey McKinnon <cmckinnon@stampinup.com>
2022-12-06 15:18:22 -0700

    day 6

---
+++ 2022/06/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Advent06
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            foreach (string line in File.ReadAllLines("data.txt"))
+            {
+                ProcessLine("Part 1: ", line, 4);
+                ProcessLine("Part 2: ", line, 14);
+            }
+        }
+
+        public static void ProcessLine(string msg, string line, int count)
+        {
+            for (int i = count; i < line.Length; i++)
+            {
+                if (line[(i - count)..i].Distinct().Count() == count)
+                {
+                    Console.WriteLine($"{msg}{i}");
+                    break;
+                }
+            }
+        }
+    }
+}
