40df9572214385d694bca25636af3c3bff19df55
Corey McKinnon <cmckinnon@stampinup.com>
2022-12-06 08:34:57 -0700

    day 3 part 2

--- 2022/03/Program.cs
+++ 2022/03/Program.cs
@@ -15,10 +15,28 @@
 
                 char shared = comp1.Intersect(comp2).First();
 
-                sum += shared - (char.IsLower(shared) ? 'a' - 1 : 'A' - 27);
+                sum += GetPriority(shared);
             }
 
             Console.WriteLine($"Part 1: {sum}");
+
+            sum = 0;
+
+            for (int i = 0; i < sacks.Length; i += 3)
+            {
+                string sack1 = sacks[i];
+                string sack2 = sacks[i + 1];
+                string sack3 = sacks[i + 2];
+
+                List<List<char>> lists = new() { sack1.ToList(), sack2.ToList(), sack3.ToList() };
+                char intersect = lists.SelectMany(x => x).Distinct().First(w => lists.TrueForAll(t => t.Contains(w)));
+
+                sum += GetPriority(intersect);
+            }
+
+            Console.WriteLine($"Part 2: {sum}");
         }
+
+        private static int GetPriority(char c) => c - (char.IsLower(c) ? 'a' - 1 : 'A' - 27);
     }
 }
