3f93e57018c67271f06bca7f4a1d227c03d3e79a
Corey McKinnon <cmckinnon@gmail.com>
2022-12-08 13:48:33 -0700

    day 8 part 2

--- 2022/08/Runner.cs
+++ 2022/08/Runner.cs
@@ -6,6 +6,13 @@ namespace Day08
         {
             string[] lines = File.ReadAllLines("datafiles/08.txt");
 
+            DoPart1(lines);
+
+            DoPart2(lines);
+        }
+
+        private static void DoPart1(string[] lines)
+        {
             HashSet<(int, int)> visibleTrees = new();
 
             // check from left
@@ -27,7 +34,7 @@ namespace Day08
             for (int i = 0; i < lines.Length; i++)
             {
                 int tallest = -1;
-                for (int j=lines[i].Length - 1; j>=0; j--)
+                for (int j = lines[i].Length - 1; j >= 0; j--)
                 {
                     int current = lines[i][j] - '0';
                     if (current > tallest)
@@ -57,7 +64,7 @@ namespace Day08
             for (int i = 0; i < lines.Length; i++)
             {
                 int tallest = -1;
-                for (int j=lines[i].Length - 1; j>=0; j--)
+                for (int j = lines[i].Length - 1; j >= 0; j--)
                 {
                     int current = lines[j][i] - '0';
                     if (current > tallest)
@@ -69,6 +76,70 @@ namespace Day08
             }
 
             Console.WriteLine($"Part 1: {visibleTrees.Count}");
+        }
+
+        private static void DoPart2(string[] lines)
+        {
+            int mapScore, highest = 0;
+
+            for (int y = 0; y < lines.Length; y++)
+            {
+                for (int x = 0; x < lines[0].Length; x++)
+                {
+                    mapScore = GetScenicScore(lines, x, y);
+                    if (mapScore > highest)
+                    {
+                        highest = mapScore;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Part 2: {highest}");
+        }
+
+        private static int GetScenicScore(string[] lines, int x, int y)
+        {
+            // edges always have a score of 0
+            if (x == 0 || y == 0 || x == lines[0].Length - 1 || y == lines.Length - 1)
+            {
+                return 0;
+            }
+
+            List<(int x, int y)> directions = new()
+            {
+                (0,-1),
+                (1,0),
+                (0,1),
+                (-1,0),
+            };
+            List<int> scores = new();
+
+            int max = lines[y][x];
+
+            foreach ((int x, int y) dir in directions)
+            {
+                int val = -1;
+                int tempScore = 0;
+                int tx = x + dir.x, ty = y + dir.y;
+
+                while (val < max && tx >= 0 && tx < lines.Length && ty >= 0 && ty < lines[0].Length)
+                {
+                    val = lines[ty][tx];
+                    tempScore++;
+                    tx += dir.x;
+                    ty += dir.y;
+                }
+                if (tempScore > 0)
+                {
+                    scores.Add(tempScore);
+                }
+            }
+
+            if (scores.Count > 0)
+            {
+                return scores.Aggregate((acc, x) => acc * x);
+            }
+            return 0;
         }
     }
 }
