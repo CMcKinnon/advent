b6f4c890c9eb078961f75aa62f8cb27ffee91b07
Corey McKinnon <cmckinnon@gmail.com>
2022-12-15 07:32:16 -0700

    day 13 part 2

--- 2022/13/Runner.cs
+++ 2022/13/Runner.cs
@@ -8,7 +8,7 @@ namespace Day13
         {
             string text = File.ReadAllText("datafiles/13.txt");
 
-            var input = text.Split($"{Environment.NewLine}{Environment.NewLine}")
+            IEnumerable<(JsonElement first, JsonElement second)> input = text.Split($"{Environment.NewLine}{Environment.NewLine}")
                 .Select(p => p.Split(Environment.NewLine))
                 .Select(p => (first: JsonDocument.Parse(p[0]).RootElement, second: JsonDocument.Parse(p[1]).RootElement));
 
@@ -25,6 +25,22 @@ namespace Day13
             }
 
             Console.WriteLine($"Part 1: {sum}");
+
+            IOrderedEnumerable<JsonElement> part2input = input
+                .SelectMany(p => new JsonElement[] {p.first, p.second})
+                .Append(JsonDocument.Parse("[[2]]").RootElement)
+                .Append(JsonDocument.Parse("[[6]]").RootElement)
+                .OrderBy(p => p, Comparer<JsonElement>.Create((f, s) => ComparePair((f, s))));
+
+            int div1 = part2input
+                .Select((p, i) => (pkt: p, idx: i + 1))
+                .First(i => ComparePair((i.pkt, JsonDocument.Parse("[[2]]").RootElement)) == 0).idx;
+
+            int div2 = part2input
+                .Select((p, i) => (pkt: p, idx: i + 1))
+                .First(i => ComparePair((i.pkt, JsonDocument.Parse("[[6]]").RootElement)) == 0).idx;
+
+            Console.WriteLine($"Part 2: {div1 * div2}");
         }
 
         private int ComparePair((JsonElement first, JsonElement second) pair)
